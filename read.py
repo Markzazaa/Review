f01d6e05b281cc2880e284eff3504ee38246e736
Mark Tseng <amaranthzazaa@hotmail.com>
2020-06-09 15:38:08 +0800

    message calculate

--- read.py
+++ read.py
@@ -7,4 +7,17 @@ with open ('reviews.txt', 'r') as f:
 		if count % 1000 == 0:  #餘數才列印
 			print (len(data))
 
+print ('讀取結束, 總共有', len(data), '筆資料')
+
+
+sum_1 = 0
+for d in data:
+	sum_1 = sum_1 + len(d)
+	
+print ('每筆留言長度為', sum_1 / len(data) )
+	
+
+
+
+
 
